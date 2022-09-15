51fbe000c6365b3f3d206d3a73d2365d1111b2f1
Esraq <esraq26552@gmail.com>
2022-09-15 12:41:05 +0600

    get & post

---
+++ lab/example.php
@@ -0,0 +1,17 @@
+<?php
+
+
+  $name="Esraq";
+  $id="17-90461-3";
+  $age=25;
+  $cgpa=3.75;
+ 
+  
+ echo "Name:".$name."<br>";
+ echo "ID:".$id."<br>";
+ echo "Name:".$age."<br>";
+ echo "Name:".$cgpa."<br>";
+
+
+
+?>
---
+++ lab/form.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$firstname=$_POST['firstname'];
+$lastname=$_POST['lastname'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+$email=$_POST['email'];
+
+
+echo "First Name:".$firstname."<br>";
+echo "lastname:".$lastname."<br>";
+echo "Username",$username."<br>";
+
+
+
+
+?>
